352d1365410cd3d863d8a524ea8e293a4e305c69
Joaquin Wienberg <joaquin_wienberg@hotmail.com>
2022-03-15 13:12:27 -0300

    Renamed NavBar.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import ResponsiveAppBar from './components/navbar';
+import ResponsiveAppBar from './components/NavBar';
 
 function App() {
   return ( 
